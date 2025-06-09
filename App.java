27cfd4a34226c2641aa356fd89fee655f559fe50
para-docx <frozendecember2001@gmail.com>
2025-06-09 18:17:02 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is a sample Java application for CI/CD." );
+    }
+}
+
+
